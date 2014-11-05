e3fdefa8237d92a67ca73acef2ec77a00ab1995a
steffen.druesedow@telekom.de <steffen.druesedow@telekom.de>
2014-11-05 10:27:40 +0100

    first attempt to use require.js for loading of MessagingStubs in order to have separated namespaces with the stubs

--- src/libs/Identity.js
+++ src/libs/Identity.js
@@ -7,7 +7,7 @@
  * @author Kay Haensge <Kay.Haensge@telekom.de>
  * @author Luis Oliveira <luis-f-oliveira@ptinovacao.pt>
  */
-    
+
 /*
  * TODO: State-machine for transitions of IdentityStatus should be described
  * TODO: Questions about MessagingStub-Listeners 
@@ -25,25 +25,25 @@
  * This constructor will throw an exception if it is used directly.
  */
 function Identity(rtcIdentity, idpRtcIdentity) {
-	console.log("idpRtcIdentity",idpRtcIdentity)
-	if ( rtcIdentity )
+	console.log("idpRtcIdentity", idpRtcIdentity)
+	if (rtcIdentity)
 		throw "Illegal attempt to create an Identity --> Please use Idp.getInstance().createIdentity(...) instead."
-	
+
 	// We use a string, RTCIdentityAssertion is not available.
 	this.rtcIdentity = idpRtcIdentity;
 	this.id = new Date().getTime();
-	console.log( "####################");
-	console.log( "created new Identiy for: " + this.rtcIdentity + " with id = " + this.id);
+	console.log("####################");
+	console.log("created new Identiy for: " + this.rtcIdentity + " with id = " + this.id);
 	console.trace();
-	console.log( "####################");
+	console.log("####################");
 	this.idp = "";
 	this.presence = {
 		status: IdentityStatus.IDLE
-		//app: this.prototype.resolveMyApp()
+				//app: this.prototype.resolveMyApp()
 	};
-	
+
 	// TODO: initialise presence status
-    //this.presence.status = IdentityStatus.IDLE; // not initialized yet TODO: do state changes in a central place
+	//this.presence.status = IdentityStatus.IDLE; // not initialized yet TODO: do state changes in a central place
 	//this.presence.app = this.prototype.resolveMyApp(); // not initialized yet TODO: do state changes in a central place
 
 	//this.status = IdentityStatus.IDLE; // TODO: to be replaced with Identity.presence
@@ -51,7 +51,7 @@ function Identity(rtcIdentity, idpRtcIdentity) {
 
 
 	this.sessionId = "";
-	
+
 	this.messagingStubLibUrl = "";
 	this.messagingStub;
 	this.notificationAddress;
@@ -68,11 +68,11 @@ function Identity(rtcIdentity, idpRtcIdentity) {
  * @returns AppType ... private function to resolve the type of Application used by the user 
  * 
  */
-Identity.prototype.resolveMyApp = function() {
-    // TODO: to check what kind of App is used:
+Identity.prototype.resolveMyApp = function () {
+	// TODO: to check what kind of App is used:
 	//     - Case is Web App running in a mobile browser it should return AppType.MOBILE_WEB_APP
 	//     - Case is Mobile Web App running in a pc browser it should return AppType.WEB_APP
-	
+
 };
 
 /**
@@ -82,54 +82,66 @@ Identity.prototype.resolveMyApp = function() {
  * 
  * @param callback {callback(MessagingStub)} callback that is invoked with messagingStub as param; if download failed then the stub param is empty
  */
-Identity.prototype.resolve = function( callback ) {
+Identity.prototype.resolve = function (callback) {
 	var that = this;
-	console.log( "resolving identity: " + this.rtcIdentity);
-	if ( ! this.messagingStub.impl ) {
+	console.log("resolving identity: " + this.rtcIdentity);
+	if (!this.messagingStub.impl) {
 		// not resolved yet --> let's ask Idp for a stub with the same downloadUri
 		var knownStub = Idp.getInstance().getResolvedStub(this.messagingStubLibUrl);
-		if ( knownStub) {
+		if (knownStub) {
 			this.messagingStub = knownStub;
 			callback(this.messagingStub);
 			return;
 		}
-		
-		console.log( "downloading Messaging stub from: " + this.messagingStubLibUrl );
 
-		// parse the downloadURL to get the name of the Stub
-		var pathArr = this.messagingStubLibUrl.split("/");
-		var stubName = pathArr[pathArr.length-1];
-		stubName = stubName.substring(0, stubName.lastIndexOf("."));
-		console.log("stub-name is: " + stubName );
-		
-		var check = function(stub, callback, count) {
-			if ( typeof(window[stub]) == "function" ) {
-				// instantiate stub
-				var messagingStub = new window[stub]();
-				//  should be an object now
-				if ( typeof(messagingStub) == "object" ) {
-					// assign the new messagingStub object to the "impl" field of the container stub
-					that.messagingStub.setImpl(messagingStub);
-					that.messagingStub.message = "stub downloaded from: " + that.messagingStubLibUrl;
-					// return container-stub in callback
-					callback(that.messagingStub);
-				}
+		console.log("downloading Messaging stub from: " + this.messagingStubLibUrl);
+		require.config({
+			paths: {
+				'stub': this.messagingStubDownloadUrl
 			}
-			else {
-				count++;
-				if ( count < 20 )
-					setTimeout( check, 500,  stub, callback, count );
-				else {
-					callback(); 
-				}
-			}
-		};
-		this.loadJSfile( this.messagingStubLibUrl );
-		setTimeout( check, 100, stubName, callback, 0 );
+		});
+		require(['stub'], function (stub) {
+			// assign the new messagingStub object to the "impl" field of the container stub
+			stub.setImpl(stub);
+			stub.message = "stub downloaded from: " + that.messagingStubLibUrl;
+			// return container-stub in callback
+			callback(stub);
+		});
+
+//		// parse the downloadURL to get the name of the Stub
+//		var pathArr = this.messagingStubLibUrl.split("/");
+//		var stubName = pathArr[pathArr.length-1];
+//		stubName = stubName.substring(0, stubName.lastIndexOf("."));
+//		console.log("stub-name is: " + stubName );
+//		
+//		var check = function(stub, callback, count) {
+//			if ( typeof(window[stub]) == "function" ) {
+//				// instantiate stub
+//				var messagingStub = new window[stub]();
+//				//  should be an object now
+//				if ( typeof(messagingStub) == "object" ) {
+//					// assign the new messagingStub object to the "impl" field of the container stub
+//					that.messagingStub.setImpl(messagingStub);
+//					that.messagingStub.message = "stub downloaded from: " + that.messagingStubLibUrl;
+//					// return container-stub in callback
+//					callback(that.messagingStub);
+//				}
+//			}
+//			else {
+//				count++;
+//				if ( count < 20 )
+//					setTimeout( check, 500,  stub, callback, count );
+//				else {
+//					callback(); 
+//				}
+//			}
+//		};
+//		this.loadJSfile( this.messagingStubLibUrl );
+//		setTimeout( check, 100, stubName, callback, 0 );
 	}
 	else {
-		console.log( this.rtcIdentity + ": no need to download stub from: " + this.messagingStubLibUrl);
-		callback( this.messagingStub );
+		console.log(this.rtcIdentity + ": no need to download stub from: " + this.messagingStubLibUrl);
+		callback(this.messagingStub);
 	}
 };
 
@@ -145,7 +157,7 @@ Identity.prototype.resolve = function( callback ) {
  *            SubscriptionType ... The subscription type
  *
  */
-Identity.prototype.subscribe = function(subscriber) {
+Identity.prototype.subscribe = function (subscriber) {
 	// TODO: get messagingStub by invoking Identity.resolve() function, add Identity as messagingStub listener (including the subscription contextId)  and send SUBSCRIBE message through the MessagingStub
 	this.context = guid(); //check this
 	that = this;
@@ -155,9 +167,9 @@ Identity.prototype.subscribe = function(subscriber) {
 	console.log("Identity subscriber", subscriber);
 
 	console.log("Identity subscribe");
-	this.resolve(function(stub){
-		
-	    //message.contextId = guid();
+	this.resolve(function (stub) {
+
+		//message.contextId = guid();
 		stub.addListener(that.onMessage(that), "presence." + to, that.context);
 		stub.sendMessage(MessageFactory.createSubscribeMessage(from, to, ""));
 	});
@@ -173,7 +185,7 @@ Identity.prototype.subscribe = function(subscriber) {
  *            SubscriptionType ... The subscription type
  *
  */
-Identity.prototype.unsubscribe = function(subscriber, type) {
+Identity.prototype.unsubscribe = function (subscriber, type) {
 	// TODO: get messagingStub by invoking Identity.resolve() function and send BYE message through the MessagingStub
 	this.messagingStub.sendMessage(MessageFactory.createUnsubscribeMessage(message));
 };
@@ -186,8 +198,8 @@ Identity.prototype.unsubscribe = function(subscriber, type) {
  *            String ... The status to set
  * 
  */
- 
-Identity.prototype.setStatus = function(status, login) {
+
+Identity.prototype.setStatus = function (status, login) {
 	//var message = new Object();
 	//message.contextId = this.context;
 
@@ -197,8 +209,8 @@ Identity.prototype.setStatus = function(status, login) {
 	console.log("Identity SetStatus: ", status);
 
 	this.presence.status = status;// TODO: change to that.presence.status = status;
-	this.messagingStub.sendMessage(MessageFactory.createContextMessage(that.rtcIdentity, "", status, login, that.sessionId));	
-    // TODO: check status transitions according to IdentityStatus state machine
+	this.messagingStub.sendMessage(MessageFactory.createContextMessage(that.rtcIdentity, "", status, login, that.sessionId));
+	// TODO: check status transitions according to IdentityStatus state machine
 	// TODO: Send a CONTEXT message to address "rtcIdentity.presence"
 };
 
@@ -209,13 +221,13 @@ Identity.prototype.setStatus = function(status, login) {
  *            String ... The presence to set
  * 
  */
- 
-Identity.prototype.setPresence = function(presence) {
+
+Identity.prototype.setPresence = function (presence) {
 
 	var that = this;
 	that.presence.status = presence;
-	
-    // TODO: check status transitions according to IdentityStatus state machine
+
+	// TODO: check status transitions according to IdentityStatus state machine
 	// TODO: Send a CONTEXT message to address "rtcIdentity.presence"
 };
 
@@ -226,8 +238,8 @@ Identity.prototype.setPresence = function(presence) {
  *            String ... The context to set
  * 
  */
- 
-Identity.prototype.setContext = function(context) {
+
+Identity.prototype.setContext = function (context) {
 
 	this.context = context;
 	this.messagingStub.sendMessage(MessageFactory.createContextMessage());
@@ -241,7 +253,7 @@ Identity.prototype.setContext = function(context) {
  * @returns IdentityStatus ... gets the presence status attribute for this Identity
  * 
  */
-Identity.prototype.getStatus = function() {
+Identity.prototype.getStatus = function () {
 // TODO: change to return this.presence.status;
 	return this.presence.status;
 };
@@ -252,7 +264,7 @@ Identity.prototype.getStatus = function() {
  * @returns Presence ... gets the presence  attribute for this Identity
  * 
  */
-Identity.prototype.getPresence = function() {
+Identity.prototype.getPresence = function () {
 	return this.presence;
 };
 
@@ -265,7 +277,7 @@ Identity.prototype.getPresence = function() {
  */
 
 
-Identity.prototype.sendMessage = function(message) {
+Identity.prototype.sendMessage = function (message) {
 	this.messagingStub.sendMessage(message);
 };
 
@@ -275,7 +287,7 @@ Identity.prototype.sendMessage = function(message) {
  * @returns ContextData ... gets the context attribute for this Identity
  * 
  */
-Identity.prototype.getContext = function() {
+Identity.prototype.getContext = function () {
 	return this.context;
 };
 
@@ -285,7 +297,7 @@ Identity.prototype.getContext = function() {
  * @returns URL ... gets the URL to download the MessagingStub for this Identity
  * 
  */
-Identity.prototype.getMessagingStubDownloadUrl = function() {
+Identity.prototype.getMessagingStubDownloadUrl = function () {
 	//will be needed to the Idp 
 	return this.messagingStubDownloadUrl;
 };
@@ -297,8 +309,8 @@ Identity.prototype.getMessagingStubDownloadUrl = function() {
  * We use this callback to disconnect and unload the MessagingStub. 
  * 
  */
-Identity.prototype.onLastMessagingListener = function() {
-	if ( this.messagingStub )
+Identity.prototype.onLastMessagingListener = function () {
+	if (this.messagingStub)
 		this.messagingStub.disconnect();
 	// "undefine" messagingStub 
 	delete this.messagingStub;
@@ -309,7 +321,7 @@ Identity.prototype.onLastMessagingListener = function() {
  * TODO: This might be a security risk, because the file is executed immediately.
  * @param url ... url of the file to be loaded
  */
-Identity.prototype.loadJSfile = function(url) {
+Identity.prototype.loadJSfile = function (url) {
 	var fileref = document.createElement('script');
 	fileref.setAttribute("type", "text/javascript");
 	fileref.setAttribute("src", url);
@@ -317,9 +329,9 @@ Identity.prototype.loadJSfile = function(url) {
 		document.getElementsByTagName("head")[0].appendChild(fileref);
 };
 
-Identity.prototype.onMessage = function(message){
-	console.log("MESSAGES-->",message)
-	switch(message.type){
+Identity.prototype.onMessage = function (message) {
+	console.log("MESSAGES-->", message)
+	switch (message.type) {
 		case MessageType.CONTEXT:
 			this.setStatus(message.identityPresence.status);
 			break;
